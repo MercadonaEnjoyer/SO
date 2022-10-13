b17f2c9f253b1468ea3c9496312213342df60240
ARÁN HUARTE DOMEC <aran.huarte@estudiantat.upc.edu>
2022-10-14 00:38:28 +0200

    Primera version

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LOGIN = new System.Windows.Forms.Button();
+            this.SIGNIN = new System.Windows.Forms.Button();
+            this.QUERY1 = new System.Windows.Forms.Button();
+            this.QUERY2 = new System.Windows.Forms.Button();
+            this.QUERY3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.USERNAME = new System.Windows.Forms.TextBox();
+            this.PASSWORD = new System.Windows.Forms.TextBox();
+            this.NAME = new System.Windows.Forms.TextBox();
+            this.DATE = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DESCONECTAR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LOGIN
+            // 
+            this.LOGIN.Location = new System.Drawing.Point(391, 36);
+            this.LOGIN.Name = "LOGIN";
+            this.LOGIN.Size = new System.Drawing.Size(75, 23);
+            this.LOGIN.TabIndex = 0;
+            this.LOGIN.Text = "LOG IN";
+            this.LOGIN.UseVisualStyleBackColor = true;
+            this.LOGIN.Click += new System.EventHandler(this.LOGIN_Click);
+            // 
+            // SIGNIN
+            // 
+            this.SIGNIN.Location = new System.Drawing.Point(391, 87);
+            this.SIGNIN.Name = "SIGNIN";
+            this.SIGNIN.Size = new System.Drawing.Size(75, 23);
+            this.SIGNIN.TabIndex = 1;
+            this.SIGNIN.Text = "SIGN IN";
+            this.SIGNIN.UseVisualStyleBackColor = true;
+            this.SIGNIN.Click += new System.EventHandler(this.SIGNIN_Click);
+            // 
+            // QUERY1
+            // 
+            this.QUERY1.Location = new System.Drawing.Point(391, 156);
+            this.QUERY1.Name = "QUERY1";
+            this.QUERY1.Size = new System.Drawing.Size(321, 40);
+            this.QUERY1.TabIndex = 2;
+            this.QUERY1.Text = "PARTIDA MAS LARGA EN UNA FECHA DETERMINADA";
+            this.QUERY1.UseVisualStyleBackColor = true;
+            this.QUERY1.Visible = false;
+            this.QUERY1.Click += new System.EventHandler(this.QUERY1_Click);
+            // 
+            // QUERY2
+            // 
+            this.QUERY2.Location = new System.Drawing.Point(391, 202);
+            this.QUERY2.Name = "QUERY2";
+            this.QUERY2.Size = new System.Drawing.Size(321, 47);
+            this.QUERY2.TabIndex = 3;
+            this.QUERY2.Text = "JUGADOR CON MAS PARTIDAS EN UNA FECHA DETERMINADA";
+            this.QUERY2.UseVisualStyleBackColor = true;
+            this.QUERY2.Visible = false;
+            this.QUERY2.Click += new System.EventHandler(this.QUERY2_Click);
+            // 
+            // QUERY3
+            // 
+            this.QUERY3.Location = new System.Drawing.Point(391, 255);
+            this.QUERY3.Name = "QUERY3";
+            this.QUERY3.Size = new System.Drawing.Size(321, 41);
+            this.QUERY3.TabIndex = 4;
+            this.QUERY3.Text = "WINRATIO DE JUGADOR DETERMINADO EN UNA FECHA DETERMINADA";
+            this.QUERY3.UseVisualStyleBackColor = true;
+            this.QUERY3.Visible = false;
+            this.QUERY3.Click += new System.EventHandler(this.QUERY3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "USUARIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "CONTRASEÑA";
+            // 
+            // USERNAME
+            // 
+            this.USERNAME.Location = new System.Drawing.Point(24, 38);
+            this.USERNAME.Name = "USERNAME";
+            this.USERNAME.Size = new System.Drawing.Size(100, 20);
+            this.USERNAME.TabIndex = 7;
+            // 
+            // PASSWORD
+            // 
+            this.PASSWORD.Location = new System.Drawing.Point(24, 85);
+            this.PASSWORD.Name = "PASSWORD";
+            this.PASSWORD.Size = new System.Drawing.Size(100, 20);
+            this.PASSWORD.TabIndex = 8;
+            // 
+            // NAME
+            // 
+            this.NAME.Location = new System.Drawing.Point(24, 158);
+            this.NAME.Name = "NAME";
+            this.NAME.Size = new System.Drawing.Size(100, 20);
+            this.NAME.TabIndex = 9;
+            this.NAME.Visible = false;
+            // 
+            // DATE
+            // 
+            this.DATE.Location = new System.Drawing.Point(24, 214);
+            this.DATE.Name = "DATE";
+            this.DATE.Size = new System.Drawing.Size(100, 20);
+            this.DATE.TabIndex = 10;
+            this.DATE.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "CONSULTAS";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "NOMBRE";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(152, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "FECHA (DD/MM/YYYY)";
+            this.label5.Visible = false;
+            // 
+            // DESCONECTAR
+            // 
+            this.DESCONECTAR.Location = new System.Drawing.Point(599, 38);
+            this.DESCONECTAR.Name = "DESCONECTAR";
+            this.DESCONECTAR.Size = new System.Drawing.Size(113, 23);
+            this.DESCONECTAR.TabIndex = 14;
+            this.DESCONECTAR.Text = "DESCONECTAR";
+            this.DESCONECTAR.UseVisualStyleBackColor = true;
+            this.DESCONECTAR.Click += new System.EventHandler(this.DESCONECTAR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DESCONECTAR);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DATE);
+            this.Controls.Add(this.NAME);
+            this.Controls.Add(this.PASSWORD);
+            this.Controls.Add(this.USERNAME);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.QUERY3);
+            this.Controls.Add(this.QUERY2);
+            this.Controls.Add(this.QUERY1);
+            this.Controls.Add(this.SIGNIN);
+            this.Controls.Add(this.LOGIN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LOGIN;
+        private System.Windows.Forms.Button SIGNIN;
+        private System.Windows.Forms.Button QUERY1;
+        private System.Windows.Forms.Button QUERY2;
+        private System.Windows.Forms.Button QUERY3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox USERNAME;
+        private System.Windows.Forms.TextBox PASSWORD;
+        private System.Windows.Forms.TextBox NAME;
+        private System.Windows.Forms.TextBox DATE;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button DESCONECTAR;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Socket server;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void LOGIN_Click(object sender, EventArgs e)//iniciar sesion 
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+
+                
+
+                string login = "0" + "-" + USERNAME.Text + "-" + PASSWORD.Text;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(login);
+
+                server.Send(mensaje);
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                login = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                if (login == "0-0")
+                {
+                    MessageBox.Show("Has accedido a la cuenta");
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    label5.Visible = true;
+                    NAME.Visible = true;
+                    DATE.Visible = true;
+                    QUERY1.Visible = true;
+                    QUERY2.Visible = true;
+                    QUERY3.Visible = true;
+                }
+                MessageBox.Show("mensaje de log in");
+                if (login == "Error")
+                {
+                    MessageBox.Show("Usuario o contraseña incorrectos");
+                }
+
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("NO HA SIDO POSIBLE CONECTARSE AL SERVIDOR");
+                return; 
+            }
+        }
+
+        private void SIGNIN_Click(object sender, EventArgs e)//registrarse
+        {
+
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+
+                string SIGNIN = "4" + "-" + USERNAME.Text + "-" + PASSWORD.Text;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(SIGNIN);
+
+                server.Send(mensaje);
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                SIGNIN = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                if (SIGNIN == "4-0")
+                {
+                    MessageBox.Show("Registrado correctamente");
+                }
+                MessageBox.Show("mensaje de sign in");
+                if (SIGNIN == "ERROR")
+                {
+                    MessageBox.Show("Ha habido un problema con la creación de cuenta");
+                }
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("NO HA SIDO POSIBLE CONECTARSE AL SERVIDOR");
+                return;
+            }
+        }
+
+        private void QUERY1_Click(object sender, EventArgs e)//jugadores de la partida mas larga
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+
+                string QUERY1 = "1" + "-" + DATE.Text;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(QUERY1);
+
+                server.Send(mensaje);
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                QUERY1 = Encoding.ASCII.GetString(msg2).Split('\0')[0];//2 o 4 nombres separados por -
+                if (QUERY1 != "Error") 
+                {
+                    int i = 1;
+                    string jugadores = "Los jugadores que jugaron la partida mas larga son : " + QUERY1.Split('-')[i];
+                    i++;
+                    while (i < QUERY1.Split('-').Length)
+                    {
+                        jugadores = jugadores + " ," + QUERY1.Split('-')[i];
+                        i++;
+                    }
+                    MessageBox.Show(jugadores);
+                }
+                if (QUERY1 == "Error")
+                {
+                    MessageBox.Show("No hay datos coincidentes");
+                }
+
+
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("NO HA SIDO POSIBLE CONECTARSE AL SERVIDOR");
+                return;
+            }
+        }
+
+        private void QUERY2_Click(object sender, EventArgs e)// nombre del jugador con mas partidas 
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+
+                string QUERY2 = "2" + "-" + DATE.Text;
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(QUERY2);
+
+                server.Send(mensaje);
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                QUERY2 = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                string mensaje2 = QUERY2.Split('-')[1];
+                if (QUERY2 != "Error")
+                {
+                    MessageBox.Show("El jugador " + mensaje2 + " fue el jugador con mas partidas el dia " + DATE.Text);
+                }
+                if (QUERY2 == "Error")
+                {
+                    MessageBox.Show("No hay datos coincidentes");
+                }
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("NO HA SIDO POSIBLE CONECTARSE AL SERVIDOR");
+                return;
+            }
+        }
+
+        private void QUERY3_Click(object sender, EventArgs e)//winratio de un jugador un dia
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+
+                string QUERY3 = "3" + "-" + NAME.Text + "-" + DATE.Text;//probar con 03/10/2022
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(QUERY3);
+
+                server.Send(mensaje);
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                QUERY3 = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                if (QUERY3 != "Error")
+                {
+                    double Wins = Convert.ToDouble(QUERY3.Split('-')[1]);
+                    double Played = Convert.ToDouble(QUERY3.Split('-')[2]);
+                    double wr; 
+                    wr = (Wins/Played) * 100; 
+                    MessageBox.Show("El jugador " + NAME.Text + " tuvo un winratio de " + wr + "% el dia " + DATE.Text);
+                }
+                if (QUERY3 == "Error")
+                {
+                    MessageBox.Show("No hay datos coincidentes");
+                }
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("NO HA SIDO POSIBLE CONECTARSE AL SERVIDOR");
+                return;
+            }
+        }
+
+        private void DESCONECTAR_Click(object sender, EventArgs e)//desconectar
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+
+                string CERRAR = "5" + "-";
+                byte[] mensaje = System.Text.Encoding.ASCII.GetBytes(CERRAR);
+
+                server.Send(mensaje);
+                server.Shutdown(SocketShutdown.Both);
+                this.BackColor = Color.Gray;
+                server.Close();
+                MessageBox.Show("DESCONECTADO DEL SERVIDOR");
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("DESCONECTADO DEL SERVIDOR");
+                return;
+            }
+        }
+    }    
+}
